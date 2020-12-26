759fdf0c2272fb263b77ac98aa044a21b9d73eca
DESKTOP-GMH0RTO\Colin <Jorgensenchj22@gmail.com>
2020-12-26 14:09:28 -0700

    Update Health.cs
    update

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -6,7 +6,7 @@ public class Health : MonoBehaviour
 {
     public int currentHealth;
     bool IsDead;
-    public int currentStamina
+    public int currentStamina;
 
     // Start is called before the first frame update
     void Start()
