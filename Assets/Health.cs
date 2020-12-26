b958257ef18ee640a6b583cddad0952b3343ed13
DESKTOP-GMH0RTO\Colin <Jorgensenchj22@gmail.com>
2020-12-26 13:37:14 -0700

    12/26/20

---
+++ Assets/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int currentHealth;
+    bool IsDead;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        print("death");
+    }
+}
